79dba647755115968dc0abd57532fcd677752e23
Christian Westbye <christian100.kodehode@gmail.com>
2023-01-20 14:49:41 +0100

    Added gsap

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./Hero.css";
+import { gsap, Timeline, Power3 } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useRef, useLayoutEffect, useEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
+  let tl = gsap.timeline();
+
+  useEffect(() => {
+    tl.from(".content", 1, { x: -400, y: 0, duration: 5, opacity: 0, ease: "ease-in", delay: 0.5 });
+    tl.to(".content", 1, { x: 0, y: 0, duration: 2 });
+    // tl.from(".textLine2", 1, { x: 400, y: 0, duration: 2, opacity: 0, ease: "ease-in", delay: 0.2 });
+    // tl.to(".textLine2", 1, { x: 0, y: 0, duration: 2 });
+  }, []);
+
   return (
     <div className="hero">
       <div className="content">
-        <p>Programmerer, med IT support bakgrunn (IKT fagbrev).</p>
-        <p>Nyutdannet med fokus på React og JS(X)!</p>
+        <p className="textLine1">Programmerer, med IT support bakgrunn (IKT fagbrev).</p>
+        <p className="textLine2">Nyutdannet med fokus på React og JS(X)!</p>
         <p>Familiemann med kone og to barn.</p>
         <p>Ta kontakt for en prat!</p>
 
