60b27506b364b401c5fe9c8734c89c06f19b2a26
Christian Westbye <christian100.kodehode@gmail.com>
2023-02-02 10:45:13 +0100

    updates

--- src/Txt/txtConst.js
+++ src/Txt/txtConst.js
@@ -5,10 +5,19 @@ export const headLineMain = {
     main2: "Liker nye utfordringer og å være kreativ både på egen hånd og på lag sammen med gode kolleger.",
     about:
       "Datamaskiner har vært med meg siden jeg var en liten gutt på 80-tallet. Først C64 og 286 videre til min første egne maskin, en Amiga 1200.",
+    about2:
+      "Utover dette så er det familie, musikk, bildebehandling og skateboard som er de store interessene. Under viser jeg et lite utvalg av diverse cover jeg har laget.",
+    // about3:
+    //   "Driver også et eget plateselskap der jeg gir ut elektronisk musikk, over 800 sanger og snart 1000 videoer på Youtube. Selskapet heter Horrordelic Records. Lager en mendge cover og flyere for dette prosjektet.",
+    about4: (
+      <a className="infoLinks" href="https://horrordelic.bandcamp.com/" alt="Horrordelic Bandcamp" target="_blank">
+        Sjekk ut Horrordelic Records
+      </a>
+    ),
     info1: "Studerer på kodehode  som Frontend utvikler med React og Js(X) som hovedfokus.",
     info3: "Jobbet med IT siden 2000 tallet på Servicdesk for blant annet Evry og Aker Kværner. Firstline + Secondline + Monitoring (IKT fagbrev).",
     info2:
-      "Nylig jobbet to år i spill firmaet Misc Games som Community Manager og Support Tech (PC, PS4, Xbox og Switch). Samt level design og feilsøking i Unreal 4.",
+      "Nylig jobbet to år i spill firmaet  Misc Games som Community Manager og Support Tech (PC, PS4, Xbox og Switch). Samt level design og feilsøking i Unreal 4.",
     info4: "Nå er det programmering som tar det meste av tiden og interessen vokser bare med kunnskapsnivået!",
   },
 };
